ce2c0f9a78df16877e11a631b03c618a39fcd29f
N3rdWizard <96712684+N3rdWizard@users.noreply.github.com>
2024-06-19 12:15:35 -0500

    Added level select
    level select working, level one and back buttons working at the moment.

---
+++ Scripts/Level Select.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+
+
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Main_Menu.tscn")
+
+
+func _on_level_1_pressed():
+	get_tree().change_scene_to_file("res://Scenes/level official 1.tscn")
--- Scripts/Main_Menu.gd
+++ Scripts/Main_Menu.gd
@@ -8,7 +8,7 @@ func _on_exit_pressed():
 	
 
 func _on_level_select_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/Level Select.tscn")
 
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://Scenes/level official 1.tscn")
